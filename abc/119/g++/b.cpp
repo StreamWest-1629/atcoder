ace85050f261edc1ef7c7d318f3c0030a1af6599
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-04-28 10:18:07 +0900

    119B - Solved

--- abc/119/g++/b.cpp
+++ abc/119/g++/b.cpp
@@ -2,4 +2,20 @@
 using namespace std;
 
 int main() {
+    int n;
+    double yen, btc;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        double d;
+        string s;
+        cin >> d >> s;
+        if (s == "JPY") {
+            yen += d;
+        }
+        else {
+            btc += d;
+        }
+    }
+    cout << (double)yen + (btc * 380000.0) << endl;
+    cout << "";
 }
