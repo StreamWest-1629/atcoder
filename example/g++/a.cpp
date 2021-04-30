f6a172990299384d060e8ce6360c12cc92feee16
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-04-30 12:32:26 +0900

    基礎実装にfor系マクロを追加

--- example/g++/a.cpp
+++ example/g++/a.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define for(type, val, n) for(type val = 0; val < n; ++val)
+#define fori(type, val, init, end) for(type val = init; val < n; ++val)
+
 int main() {
 }
