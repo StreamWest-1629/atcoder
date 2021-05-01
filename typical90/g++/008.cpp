a3a1eddd66363ecc46b4c1f3b6416848cd9fb9ac
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-05-01 19:19:12 +0900

    typical90-008 - Solved

--- typical90/g++/008.cpp
+++ typical90/g++/008.cpp
@@ -2,41 +2,38 @@
 using namespace std;
 
 #define rep(type, val, n) for(type val = 0; val < n; ++val)
-#define repi(type, val, init, end) for(type val = init; val < n; ++val)
+#define repi(type, val, init, end) for(type val = init; val < end; ++val)
 #define band(a, b) pair<int, int>(a, b)
 
 int main() {
     int n;
     string s;
-    vector<long> str_i[6];
+    vector<long> str_i[7];
     auto atcoder = "atcoder";
     cin >> n;
-    rep(int, i, 6) {
+    rep(int, i, 7) {
         str_i[i] = vector<long>(n);
         rep(int, j, n) 
             str_i[i][j] = 0;
     }
     cin >> s;
 
-    rep(int, j, 6)
-        str_i[j][0] = (s[0] == atcoder[j] ? 1 : 0);
+    str_i[0][0] = s[0] == atcoder[0] ? 1 : 0;
 
-    repi(int, i, 1, n) {
-        rep(int, j, 6) 
-            str_i[j][i] = str_i[j][i - 1] + (s[i] == atcoder[j] ? 1 : 0);
+    repi(int, i, 1, n) 
+        str_i[0][i] = str_i[0][i - 1] + (s[i] == atcoder[0] ? 1 : 0);
+    
+    repi(int, i, 1, 7) {
+        
+        str_i[i][0] *= str_i[i - 1][0];
+
+        repi(int, j, 1, n) {
+            str_i[i][j] = str_i[i][j - 1] + (s[j] == atcoder[i] ? str_i[i - 1][j] : 0);
+            str_i[i][j] %= (long)(1e9 + 7);
+        }
     }
 
-    rep(int, i, n) 
-        repi(int, j, 1, 6) {
-            str_i[j][i] *= str_i[j - 1][i];
-            str_i[j][i] %= (long)1e9 + 7;
-        }
-    
-    long ans = 0;
-    rep(int, i, n)
-        ans += (s[i] == atcoder[6] ? str_i[5][i] : 0);
-
-    ans %= (long)1e9 + 7;
-    cout << ans << endl;
+    long ans = str_i[6][n - 1];
+    cout << (ans % (long)(1e9 + 7)) << endl;
     return 0;
 }
