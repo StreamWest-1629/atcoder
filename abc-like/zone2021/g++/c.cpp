96c9eac0ef9c64495937d1625a86675463b5a307
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-05-02 10:54:09 +0900

    zone2021C - Failed

--- abc-like/zone2021/g++/c.cpp
+++ abc-like/zone2021/g++/c.cpp
@@ -10,18 +10,36 @@ int main() {
     int n, ans = 0;
     cin >> n;
     vector<int> p[5];
-    rep(int, i, 5) p[i] = vector<int>(n);
+    rep(int, i, 5) {
+        p[i] = vector<int>(n);
+    }
     
     rep(int, i, n) rep(int, j, 5)
         cin >> p[j][i];
     
-    for (int bit_i = (1 << 30); bit_i > 0; bit_i >>= 1) {
+    for (int bit_i = (1 << 29); bit_i > 0; bit_i >>= 1) {
         int res = 0;
         int buf = ans | bit_i;
-        rep(int, i, n) rep(int, j, 5)
-            res |= (p[j][i] >= buf ? (1 << j) : 0);
-        if (res == 0x1f)
-            ans = buf;
+        int array[32] = { 0 };
+
+        rep(int, i, n) {
+            int add = 0;
+            rep(int, j, 5) 
+                if (p[j][i] >= buf) {
+                    add |= (1 << j);
+                }
+            array[add]++;
+        }
+
+        rep(int, i, 30) 
+            repi(int, j, i + 1, 31)
+                repi(int, k, j + 1, 32)
+                    if ((i | j | k) == 31)
+                        if (array[i] && array[j] && array[k]) {
+                            ans = buf;
+                            goto jump;
+                        }
+        jump:;
     }
     cout << ans;
     return 0;
