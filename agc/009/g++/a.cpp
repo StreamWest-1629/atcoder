9fe84e82c21277df3f29ff769b39e78c2f6a0ec5
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-05-02 11:31:07 +0900

    agc009A - Solved

--- agc/009/g++/a.cpp
+++ agc/009/g++/a.cpp
@@ -7,4 +7,16 @@ using namespace std;
 using intpair = pair<int, int>;
 
 int main() {
+    long n, a[(int)1e5 + 5], b[(int)1e5 + 5], ans = 0;
+    cin >> n;
+    rep(int, i, n) 
+        cin >> a[i] >> b[i];
+    
+    for(int i = n - 1; i > -1; --i) {
+        int mod = ((a[i] + ans) % b[i]);
+        ans += mod == 0 ? 0 : b[i] - mod;
+    }
+
+    cout << ans;
+    return 0;
 }
