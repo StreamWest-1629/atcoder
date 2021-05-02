eb565dd17b5c894165f3884cc82e5d112984b70f
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-05-02 09:28:10 +0900

    zone2021C - Failed

--- abc-like/zone2021/g++/c.cpp
+++ abc-like/zone2021/g++/c.cpp
@@ -7,47 +7,22 @@ using namespace std;
 using intpair = pair<int, int>;
 
 int main() {
-    long n, p[3005][5], ans = 0, buf[5] = { 0 };
+    int n, ans = 0;
     cin >> n;
-    rep(int, i, n)
-        rep(int, j, 5)
-            cin >> p[i][j];
+    vector<int> p[5];
+    rep(int, i, 5) p[i] = vector<int>(n);
     
-    auto result = [&](int a, int b, int c) {
-        long res = 1e10;
-        rep(int, i, 5)
-            res = min(res, max(p[a][i], max(p[b][i], p[c][i])));
-        return res;
-    };
-
-    long a = 0, b = 1, c = 2;
-    long max = result(a, b, c);
-
-    auto maximize = [&](int toa, int tob, int toc, long val) {
-        if (val > max) {
-            a = toa;
-            b = tob;
-            c = toc;
-            max = val;
-        }
-    };
-
-    repi(int, i, 3, n) {
-        long max_a = result(i, b, c), 
-            max_b = result(a, i, c),
-            max_c = result(a, b, i);
-        if (max_a > max_b) 
-            if (max_a > max_c) 
-                maximize(i, b, c, max_a);
-            else 
-                maximize(a, b, i, max_c);
-        else
-            if (max_b > max_c)
-                maximize(a, i, c, max_b);
-            else
-                maximize(a, b, i, max_c);
+    rep(int, i, n) rep(int, j, 5)
+        cin >> p[j][i];
+    
+    for (int bit_i = (1 << 30); bit_i > 0; bit_i >>= 1) {
+        int res = 0;
+        int buf = ans | bit_i;
+        rep(int, i, n) rep(int, j, 5)
+            res |= (p[j][i] >= buf ? (1 << j) : 0);
+        if (res == 0x1f)
+            ans = buf;
     }
-
-    cout << max << endl;
+    cout << ans;
     return 0;
 }
