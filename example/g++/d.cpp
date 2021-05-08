42c37705be7f482a95d7eee64393bd28ea91825b
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-05-08 23:21:07 +0900

    32bitを使わない方向で修正

--- example/g++/d.cpp
+++ example/g++/d.cpp
@@ -4,7 +4,8 @@ using namespace std;
 #define rep(type, val, n) for(type val = 0; val < n; ++val)
 #define repi(type, val, init, end) for(type val = init; val < end; ++val)
 
-using intpair = pair<int, int>;
+using Int = long long;
+using uInt = unsigned long long;
 
 int main() {
     return 0;
