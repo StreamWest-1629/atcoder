8b72276d72490b6a66a2fb7ee668580ded167de6
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-05-10 18:50:07 +0900

    abc168D - Solved

--- abc/168/g++/d.cpp
+++ abc/168/g++/d.cpp
@@ -8,5 +8,44 @@ using Int = long long;
 using uInt = unsigned long long;
 
 int main() {
+
+    Int n, m, count = 1;
+    vector<vector<Int>> map;
+    queue<int> choice = queue<int>();
+    vector<Int> ans;
+
+    cin >> n >> m;
+    map = vector<vector<Int>>(n);
+    ans = vector<Int>(n);
+    rep(int, i, n) {
+        map[i] = vector<Int>();
+        ans[i] = -1;
+    }
+    rep(int, i, m) {
+        Int a, b;
+        cin >> a >> b;
+        a--, b--;
+        map[a].push_back(b);
+        map[b].push_back(a);
+    }
+    rep(int, i, map[0].size()) 
+        if (ans[map[0][i]] == -1) {
+            ans[map[0][i]] = 1;
+            choice.push(map[0][i]);
+        }
+    while (!choice.empty()) {
+        auto choose = choice.front();
+        choice.pop();
+        rep(int, i, map[choose].size()) 
+            if (ans[map[choose][i]] == -1) {
+                ans[map[choose][i]] = choose + 1;
+                choice.push(map[choose][i]);
+                count++;
+            }
+    }
+
+    cout << "Yes" << endl;
+    repi(int, i, 1, n)
+        cout << ans[i] << endl;
     return 0;
 }
