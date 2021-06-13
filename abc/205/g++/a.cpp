56b6cbf58b9f627d5d44101db7aa309e3c21ff8a
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-13 20:48:24 +0900

    abc205を解くためのコード準備

---
+++ abc/205/g++/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(type, val, n) for(type val = 0; val < n; ++val)
+#define repi(type, val, init, end) for(type val = init; val < end; ++val)
+
+using Int = long long;
+using uInt = unsigned long long;
+
+template<typename Ty>
+using in = const Ty&;
+
+int main() {
+    return 0;
+}
